d0b69810154dab777b5b5a051232fd8857b604f7
Brent Elisens <37149480+sixie6e@users.noreply.github.com>
2026-01-13 13:53:43 -0500

    Update shwrite.py

--- shwrite.py
+++ shwrite.py
@@ -1,5 +1,5 @@
 with open('selections_history.txt', 'r') as shread:
     with open('sh.txt', 'w+') as shwrite:
         for line in shread:
-            if "Length: 0" not in line:
+            if "Length: 0" or "Length: 1" not in line:
                 shwrite.write(line)
